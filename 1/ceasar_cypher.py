ffe1acac079577a8aca11acbfaeff07b1bb18d64
joco1026 <jsh.coh@gmail.com>
2013-03-05 16:22:19 -0500

    Update ceasar_cypher.py

--- 1/ceasar_cypher.py
+++ 1/ceasar_cypher.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
 #http://www.pythonchallenge.com/pc/def/map.html
-#This will encrypt/decrypt a phrase using Ceasar Cypher, or a cypher that offsets each letter in the alphabet by a given number.
+#This will encrypt/decrypt a phrase using Ceasar Cypher, or a cypher that offsets 
+#each letter in the alphabet by a given number.
 
 import string
 
@@ -13,6 +14,7 @@ lowletters = string.lowercase
 upletters = string.uppercase
 
 #use the maketrans method to create a translation table.
+#The steps are duplicated for uppercase letters, and then appended to lowercase.
 #maketrans(input,output)
 table = string.maketrans(lowletters + upletters, lowletters[int(step):] + lowletters[:int(step)] + upletters[int(step):] + upletters[:int(step)])
 
